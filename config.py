e0ddd64ccf12160f8aa20dcbad4903a7f018e4a4
preethamdg <preethamdgpreethamdg5@gmail.com>
2025-03-17 13:47:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ import os
 
 
 # Required Variables Config
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-ADMIN = int(os.environ.get("ADMIN", ""))
+API_ID = int(os.environ.get("API_ID", "23009724"))
+API_HASH = os.environ.get("API_HASH", "66a87d259426e268bdd765fb4c635f2b")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7824911074:AAEk7FKLig0x48wL3Evzw7-pAxJ5Gx6_lh4")
+ADMIN = int(os.environ.get("ADMIN", "7405406082"))
 
 
 # Premium 4GB Renaming Client Config
@@ -14,39 +14,19 @@ STRING_SESSION = os.environ.get("STRING_SESSION", "")
 
 
 # Log & Force Channel Config
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "kannadarockersdg")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002334874259"))
 
 
 # Mongo DB Database Config
-DATABASE_URL = os.environ.get("DATABASE_URL", "")
+DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://hello:Preethu@cluster0.isgp5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "madflixbotz")
 
 
 # Other Variables Config
-START_PIC = os.environ.get("START_PIC", "https://graph.org/file/ad48ac09b1e6f30d2dae4.jpg")
-
-
-
-
-
-# If You Need To Add Verify System Then Message Me On Telegram
-# Check Demo Bots
-# https://t.me/FileRenameXBot
-# https://t.me/PremiumRenamerRobot
-# https://t.me/FileRenamerXRobot
-# Token Verification Adding Features Is Paid So If You Want Then Dm Me
+START_PIC = os.environ.get("START_PIC", "https://envs.sh/aBy.jpg")
 
 
 # SHORTNER_URL = os.environ.get("SHORTNER_URL", "")
 # SHORTNER_API = os.environ.get("SHORTNER_API", "")
 # TOKEN_TIMEOUT = os.environ.get("TOKEN_TIMEOUT", "")
-
-
-
-
-# Jishu Developer 
-# Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Backup Channel @JishuBotz
-# Developer @JishuDeveloper
